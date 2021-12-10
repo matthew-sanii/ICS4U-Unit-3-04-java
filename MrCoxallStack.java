96f8554a08af53f6bf67c7b175077424e7d0cc0f
matthew-sanii <60710943+matthew-sanii@users.noreply.github.com>
2021-12-10 17:16:41 +0000

    edit

---
+++ MrCoxallStack.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class MrCoxallStack {
+  /**
+  * The arrayList creator called theStack.
+  */
+  private ArrayList<Integer> theStack = new ArrayList<Integer>();
+
+  /**
+  * the push method.
+  *
+  * @param pushValue the value that is being added to the stack.
+  */
+  public void push(final int pushValue) {
+    System.out.println("Adding " + pushValue + " to stack.");
+    theStack.add(pushValue);
+  }
+
+  /**
+  * The showStack method.
+  */
+  public void showStack() {
+    System.out.println("The stacklist.");
+    for (Integer aSingleElement : theStack) {
+      System.out.println(aSingleElement);
+    }
+    System.out.println("End of stacklist. \n");
+  }
+
+  /**
+  * The pop method.
+  */
+  public int pop() {
+    try {
+      int place = theStack.get(0);
+      System.out.println("Popping top element: "
+      + place);
+      theStack.remove(0);
+      return(place);
+    } catch (ArrayIndexOutOfBoundsException errorCode) {
+      System.out.println(errorCode);
+      return(0);
+    }
+  }
+
+  /**
+  * the peek method.
+  */
+  public int peek() {
+    try {
+      int place = theStack.get(0);
+      return(place);
+    } catch (ArrayIndexOutOfBoundsException errorCode) {
+      System.out.println(errorCode);
+      return(-1);
+    }
+  }
+
+  /**
+  * The clear function.
+  */
+  public void Clear() {
+    theStack.clear();
+    System.out.println("Cleared stack.");
+  }
+}
---
+++ Stack.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+final class Stack {
+
+  /**
+  * Prevent instantiation
+  * Throw an exception IllegalStateException.
+  * if this ever is called
+  *
+  * @throws IllegalStateException
+  *
+  */
+  private Stack() {
+    throw new IllegalStateException("Cannot be initated.");
+  }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args Name of file containing a string of numbers
+    */
+  public static void main(final String[] args) {
+    final Scanner userInput = new Scanner(System.in);
+    final int min = 1;
+    final int max = 10;
+    final int multi = 4;
+    int times = multi;
+    // use MrCoxallStack class
+    MrCoxallStack newStack = new MrCoxallStack();
+    while (times >= 0) {
+      final int randomizer = (int) Math.floor(Math.random() * (max - min + 1)
+      + min);
+      newStack.push(randomizer);
+      times -= 1;
+    }
+    System.out.println("What do you want to add? ");
+    try {
+      final int input = userInput.nextInt();
+      newStack.push(input);
+    } catch (InputMismatchException errorCode) {
+      System.out.println(errorCode);
+      System.exit(0);
+    }
+    newStack.showStack();
+    newStack.pop();
+    newStack.showStack();
+    try {
+      int value = newStack.peek();
+      System.out.println("Top value is: " + value);
+      newStack.Clear();
+      newStack.showStack();
+    } catch (IndexOutOfBoundsException errorCode) {
+      System.out.println(errorCode);
+      System.exit(0);
+    }
+  }
+}
